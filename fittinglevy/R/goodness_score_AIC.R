0529e522ab84da16435ed1df72194978ff4b0a17
Torsten <torsten.heinrich@posteo.net>
2019-02-25 13:14:45 +0100

    Included AIC goodness of fit measures to fitting packages based on @yangjh2612's code

---
+++ fittinglevy/R/goodness_score_AIC.R
@@ -0,0 +1,16 @@
+# Function for providing Akaike information criterion as goodness of fit measure
+
+if (!'pacman' %in% installed.packages()[,'Package']) install.packages('pacman', repos='http://cran.r-project.org')
+pacman::p_load(boot,dplyr,StableEstim)
+
+levy_AIC <- function(para_levy, observations) {
+  # Arguments:
+  #     para_levy: numeric array of size 4, four estimated parameters of the Levy distribution
+  #     observations: numeric, sample values
+  # Returns:
+  #     AIC of the Levy fit
+    
+  levy_aic <- 2*4 - sum(dstable(observations, para_levy[1], para_levy[2], para_levy[3], para_levy[4], log = T))
+
+  return(levy_aic)
+}
