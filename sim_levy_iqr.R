c78436d4667639fbcd8c589f4a4725bf15e603dc
Julian Winkler <JulianW@oxfordmartin.ox.ac.uk>
2019-03-20 11:10:19 +0000

    added comments, cleaned up a little

--- sim_levy_iqr.R
+++ sim_levy_iqr.R
@@ -23,55 +23,34 @@ iqr_tab <- iqr_tab %>%
   rowwise() %>%
   mutate(IQR = qstable(Top, a, b, g, d) - qstable(Bottom, a, b, g, d)) %>% # find the relevant IQRs for the given parameters
   ungroup() %>%
-  mutate(pc_IQR = ifelse((row_number() %% 2) == 0, (IQR - lag(IQR,1))/lag(IQR,1), NA),
+  mutate(pc_IQR = ifelse((row_number() %% 2) == 0, (IQR - lag(IQR,1))/lag(IQR,1), NA), # we want to compute the pairwise differences (row 2 vs 1, 4 vs 3 - not 3 vs 2!) - the ifelse checks whether the row is even before computing a number
          pc_IQR = round(100*pc_IQR, digit = 2)) # express as %
 
 print(xtable(iqr_tab, caption = 'Interquartile Range as a Measure of Dispersion'), include.rownames = FALSE) # paste table into latex
 # Scale of distribution is affected by gamma - direct impact on IQR, but alpha and beta can also change it
 
-x <- seq(-10, 10, 0.1)
-xx_1 <- dstable(x, alpha = 1.3, beta = 0, gamma = 2, delta = 0, pm = 0)
-xx_2 <- dstable(x, alpha = 1.3, beta = 1, gamma = 2, delta = 0, pm = 0)
-
-plot(range(x),
-     range(xx_1, xx_2),
-     type = 'n',
-     cex = 0.9,
-     xlab = 'Value', 
-     ylab = 'Density',
-     cex.axis = 1.3,
-     cex.lab = 1.3)
-lines(x,
-      xx_1,
-      lty = 1,
-      col = 'steelblue',
-      lwd = 2)
-lines(x,
-      xx_2,
-      lty = 1,
-      col = 'coral',
-      lwd = 2)
-
 # Plot IQR as a function of beta
+# First set parameter values
 
 a <- 1.5
 g <- 1
 d <- 0
-Top <- 0.99
-Bottom <- 0.1
+Top <- 0.75
+Bottom <- 0.25
 
-IQR_1 <- list()
+IQR <- list()
 
 for(i in 0:200){
-  print(i)
-  b <- (i - 100)/100
-  IQR_1[i+1] <- qstable(Top, a, b, g, d) - qstable(Bottom, a, b, g, d)
+  b <- (i - 100)/100 # these are the possible beta values
+  IQR[i+1] <- qstable(Top, a, b, g, d) - qstable(Bottom, a, b, g, d)
 }
+
+# Create plot
 
 pdf('C:/Users/JulianW/Documents/Work/Productivity Dispersion/sim_graphs/beta_IQR.pdf', height = 6, width = 8)
 
 plot(range(beta),
-     range(IQR_1),
+     range(IQR),
      type = 'n',
      cex = 0.9,
      xlab = 'Beta', 
@@ -79,33 +58,38 @@ plot(range(beta),
      cex.axis = 1.3,
      cex.lab = 1.3)
 lines(beta,
-      IQR_1,
+      IQR,
       lty = 1,
       col = 'steelblue',
       lwd = 2)
 
 dev.off()
 
-a <- 1.7
-IQR_1 <- matrix(, nrow = 201, ncol = 49)
+# Surface plots - how does the IQR change for choices of beta, alpha, and the relevant precentiles
+# Consider 3 alpha values, 1.7, 1.5, 1.2 -1 (Cauchy) will cause problems
+# Consider IQRs from 30% to 1%
 
+a <- 1.7
+IQR_1 <- matrix(, nrow = 201, ncol = 30) # the matrix has beta values as rows, and percentiles as columns
+
+# assign row and col names, which are reffered to when plotting
 rownames(IQR_1) <- (seq(1:201) - 101)/100
 colnames(IQR_1) <- paste((seq(1:49)), '%', sep = '')
 
-for(j in 1:30){
+for(j in 1:30){ # run over all IQRs
   print(j)
-  Top <- (100 - j) / 100
-  Bottom <- j / 100
+  Top <- (100 - j) / 100 # top percentile
+  Bottom <- j / 100 # bottom percentile
   
-  for(i in 1:201){
-
+  for(i in 1:201){ # compute over all betas
     b <- (i - 101)/100
     IQR_1[i, j] <- qstable(Top, a, b, g, d) - qstable(Bottom, a, b, g, d)
   }
 }
 
+# repeat for alphas 1.5 and 1.2
 a <- 1.5
-IQR_2 <- matrix(, nrow = 201, ncol = 49)
+IQR_2 <- matrix(, nrow = 201, ncol = 30)
 
 rownames(IQR_2) <- (seq(1:201) - 101)/100
 colnames(IQR_2) <- paste((seq(1:49)), '%', sep = '')
@@ -123,7 +107,7 @@ for(j in 1:30){
 }
 
 a <- 1.2
-IQR_3 <- matrix(, nrow = 201, ncol = 49)
+IQR_3 <- matrix(, nrow = 201, ncol = 30)
 
 rownames(IQR_3) <- (seq(1:201) - 101)/100
 colnames(IQR_3) <- paste((seq(1:49)), '%', sep = '')
@@ -140,53 +124,31 @@ for(j in 1:30){
   }
 }
 
-a <- 1
-IQR_4 <- matrix(, nrow = 201, ncol = 49)
+# plot_ly is used for the surface plot - it is elegant and easy to use
 
-rownames(IQR_4) <- (seq(1:201) - 101)/100
-colnames(IQR_4) <- paste((seq(1:49)), '%', sep = '')
-
-for(j in 1:30){
-  print(j)
-  Top <- (100 - j) / 100
-  Bottom <- j / 100
-  
-  for(i in 1:201){
-    
-    b <- (i - 101)/100
-    IQR_4[i, j] <- qstable(Top, a, b, g, d) - qstable(Bottom, a, b, g, d)
-  }
-}
-
-f <- list(
-  family = 'sans',
-  size = 18,
-  color = 'black'
-)
+# x and y are lists of axes options
 
 x <- list(
-  title = 'Percentile',
-  titlefont = f,
-  tickmode = 'array',
-  tickvals = (c(1, 5, 10, 15, 25) -1), # subrtact 1 because plot starts at 0
-  ticktext = colnames(IQR_1)[c(1, 5, 10, 15, 25)]
+  title = 'Percentile', # title of axis
+  tickmode = 'array', # how to mark the ticks
+  tickvals = (c(1, 5, 10, 15, 25) -1), # location for tick marks - subrtact 1 because plot starts at 0
+  ticktext = colnames(IQR_1)[c(1, 5, 10, 15, 25)] # values for the tick marks
 )
 
+# repeat for y 
 y <- list(
   title = 'beta',
-  titlefont = f,
-  tickvals = (c(1, 50, 100, 150, 201) -1), # subrtact 1 because plot starts at 0
+  tickvals = (c(1, 50, 100, 150, 201) -1),
   ticktext = rownames(IQR_1)[c(1, 51, 101, 151, 201)]
 )
 
-plot_ly(showscale = FALSE) %>%
-  add_surface(z = ~IQR_1) %>%
-  add_surface(z = ~IQR_2, opacity = 0.7) %>%
-  add_surface(z = ~IQR_3, opacity = 0.7) %>%
-  # add_surface(z = ~IQR_4, opacity = 0.7) %>% # The Cauchy does not work well - there were warnings from the calculations, but this is not entirely surprising
+plot_ly(showscale = FALSE) %>% # data will be added seperately since three plots should be combined
+  add_surface(z = ~IQR_1) %>% # first plot - alpha = 1.7
+  add_surface(z = ~IQR_2, opacity = 0.7) %>% # first plot - alpha = 1.7 - add opacity to make is easier to see
+  add_surface(z = ~IQR_3, opacity = 0.7) %>% # first plot - alpha = 1.7
   layout(
     title = "Skew and IQRs",
-    scene = list(
+    scene = list( # this is the option that allows manipulation of axis titles, tick marks, camera angle, etc
       xaxis = x,
       yaxis = y,
       zaxis = list(title = 'IQR')
